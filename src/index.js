e90a6b6615e37f0dabce34828055eabc38d87b50
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-09-03 13:44:56 -0700

    Add dev flag check.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ import { addAction, addFilter } from '@wordpress/hooks';
 import correctTextFontWeight from './text-font-weight-correct';
 import setupJetpackEditor from './jetpack-editor-setup';
 
-
 addAction( 'native.pre-render', 'gutenberg-mobile', ( props ) => {
 	correctTextFontWeight();
 	setupJetpackEditor(
 		props.jetpackState || { blogId: 1, isJetpackActive: true }
 	);
-	// I am not sure this is the right way to do this. 
-	// But if I do an import instead we end up with an error.
-	require( './block-experiments-setup' );
+	if ( __DEV__ ) {
+		// I am not sure this is the right way to do this. 
+		// But if I do an import instead we end up with an error.
+		require( './block-experiments-setup' );
+	}
 } );
 
 addFilter( 'native.block_editor_props', 'gutenberg-mobile', ( editorProps ) => {
